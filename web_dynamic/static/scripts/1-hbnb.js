7fd78e85aa4c15abeb1355122ad691a4a2eac8e2
papihoudini <ochiengcliff.co@gmail.com>
2023-08-06 14:52:26 -0400

    Select some amenities to be comfortable

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,24 +1,23 @@
 // JavaScript script that is executed only when DOM is loaded
 // Uses JQuery
 
-let checked_box = {};
+const checkedBox = {};
 $(document).ready(function () {
-    $('input:checkbox').change(function () {
-	if ($(this).is(':checked_box')) {
-	    checked_box[$(this).data('id')] = $(this).data('name');
-	}
-	else {
-	    delete checked_box[$(this).data('id')];
-	}
-	$('div.amenities h4').html(function () {
-	    let amenities = [];
-	    Object.keys(checked_box).forEach(function (key) {
-		amenities.push(checked_box[key]);
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked_box')) {
+	    checkedBox[$(this).data('id')] = $(this).data('name');
+    } else {
+	    delete checkedBox[$(this).data('id')];
+    }
+    $('div.amenities h4').html(function () {
+	    const amenities = [];
+	    Object.keys(checkedBox).forEach(function (key) {
+        amenities.push(checkedBox[key]);
 	    });
 	    if (amenities.length === 0) {
-		return ('&nbsp');
+        return ('&nbsp');
 	    }
 	    return (amenities.join(', '));
-	});
     });
+  });
 });
